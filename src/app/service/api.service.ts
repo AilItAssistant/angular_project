131f4a61d884ee6e9c62bc0803913242531ccf7b
AilItAssistant <marketingassistant@ailmadrid.com>
2024-07-17 13:42:31 +0200

    connected with node

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  API_SERVER = "http://localhost:4000/api";
+
+  constructor(private http: HttpClient) { }
+
+  public getApi(api: string): Observable <any> {
+    return this.http.get<any>(this.API_SERVER + api)
+  }
+}

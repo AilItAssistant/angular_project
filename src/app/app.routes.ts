7830baf8dbd158da515d8c597a4c1d2f15c012bd
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-06 14:34:14 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AlumnosComponent } from './pages/alumnos/alumnos.component';
 import { ExamsComponent } from './pages/exams/exams.component';
 import { AccommodationsComponent } from './pages/accommodations/accommodations.component';
 import { AddQuestionsComponent } from './pages/add-questions/add-questions.component';
@@ -24,10 +23,6 @@ export const routes: Routes = [
     {
         path: "",
         component: HomeComponent
-    },
-    {
-        path: "alumnos",
-        component: AlumnosComponent
     },
     {
         path: "exams",

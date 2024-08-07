3d96679eb8c5a8e674b8b7773149623906f418f1
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-07 10:50:08 +0200

    students

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -11,4 +11,31 @@ import { FooterComponent } from '../../components/footer/footer.component';
 })
 export class StudentsComponent {
 
+  students: any = [
+    {
+      id: 1,
+      name: "José Carlos",
+      surname: "Fernández Giménez",
+      document: "x9865553214x",
+      birthday: "2000-03-03",
+      mobile: "+659635214587",
+      email: "marketingassistant@ailmadr55id.com",
+      status: true,
+      city: "Vallecas",
+      genre: "hombre"
+    },
+    {
+      id: 2,
+      name: "María Fernanda",
+      surname: "González Perez",
+      document: "x9865557414x",
+      birthday: "2000-03-03",
+      mobile: "+654985645132",
+      email: "marketingassistant@ailma55drid.com",
+      status: false,
+      city: "Principado de Mónaco",
+      genre: "mujer"
+    }
+  ];
+
 }

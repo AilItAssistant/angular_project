37dd541f72768cc50ab955bdd80b7dd77855509b
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-07 12:56:34 +0200

    add students

--- src/app/pages/manage-students/manage-students.component.ts
+++ src/app/pages/manage-students/manage-students.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-manage-students',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './manage-students.component.html',
   styleUrl: './manage-students.component.scss'
 })
@@ -37,5 +39,17 @@ export class ManageStudentsComponent {
       genre: "mujer"
     }
   ];
+
+  addStudentsForm = new FormGroup({
+    name: new FormControl(""),
+    surname: new FormControl(""),
+    document: new FormControl(""),
+    birthday: new FormControl(""),
+    mobil: new FormControl(""),
+    email: new FormControl(""),
+    city: new FormControl(""),
+    genre: new FormControl(""),
+    status: new FormControl(""),
+  });
 
 }

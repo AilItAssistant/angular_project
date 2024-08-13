94bdfa9faf1eda23f3b088c2267978afc7850bbb
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-13 10:26:16 +0200

    manage teachers add functionallity

--- src/app/pages/manage-teachers/manage-teachers.component.ts
+++ src/app/pages/manage-teachers/manage-teachers.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-teachers',
@@ -12,6 +13,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class ManageTeachersComponent {
 
+  deleteVariables: any = {};
+  charge: boolean = false;
   teachers: any = [
     {
       id: 1,
@@ -74,10 +77,112 @@ export class ManageTeachersComponent {
   addTeachersForm = new FormGroup({
     name: new FormControl(""),
     surname: new FormControl(""),
-    document: new FormControl(""),
-    mobil: new FormControl(""),
+    mobile: new FormControl(""),
     email: new FormControl(""),
     status: new FormControl(""),
   });
+
+  editTeachersForm = new FormGroup({
+    name: new FormControl(""),
+    surname: new FormControl(""),
+    mobile: new FormControl(""),
+    email: new FormControl(""),
+    status: new FormControl(""),
+  });
+
+  constructor(private http: HttpClient) {}
+
+  addTeacher(){
+    let teacher: any = {
+      name: this.addTeachersForm.value.name,
+      surname: this.addTeachersForm.value.surname,
+      mobil: this.addTeachersForm.value.mobile,
+      email: this.addTeachersForm.value.email,
+      status: this.addTeachersForm.value.status,
+    };
+    
+    console.log(this.addTeachersForm.value);
+
+    this.http.post<any>('http://localhost:4000/api/teachers', teacher).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        //alert('Cargar fallo' + err);
+      },
+      });
+  };
+
+  openDeleteModal(teacher: any){
+    let deleteModal: any;
+    deleteModal = document.getElementById('deleteModal');
+    deleteModal.style.display="block";
+    this.deleteVariables = teacher;
+  };
+
+  delete(){
+    console.log(this.deleteVariables.id)
+    this.http.delete<any>('http://localhost:4000/api/teachers', this.deleteVariables.id).subscribe({
+      next: (res) => {
+        console.log(res)
+        let deleteModal: any;
+        deleteModal = document.getElementById('deleteModal');
+        deleteModal.style.display="none";
+      },
+      error: (err) => {
+        //alert('Cargar fallo' + err);
+      },
+    });
+  };
+
+  closeEditModal(){
+    let editModal: any;
+    editModal = document.getElementById('editModal');
+    editModal.style.display="none";
+  };
+
+  closeDeleteModal(){
+    let deleteModal: any;
+    deleteModal = document.getElementById('deleteModal');
+    deleteModal.style.display="none";
+  };
+
+  modify(){
+
+    let mod: any = {
+      name: this.editTeachersForm.value.name,
+      surname: this.editTeachersForm.value.surname,
+      mobile: this.editTeachersForm.value.mobile,
+      email: this.editTeachersForm.value.email,
+      status: this.editTeachersForm.value.status,
+      action: "modify"
+    };
+  
+    this.http.put<any>('http://localhost:4000/api/teachers', mod).subscribe({
+      next: (res) => {
+        console.log(res)
+        let editModal: any;
+        editModal = document.getElementById('editModal');
+        editModal.style.display="none";
+      },
+      error: (err) => {
+        //alert('Cargar fallo' + err);
+      },
+    });
+  };
+
+  openEditModal(teacher: any){
+    this.editTeachersForm = new FormGroup({
+      name: new FormControl(teacher.name),
+      surname: new FormControl(teacher.surname),
+      mobile: new FormControl(teacher.mobile),
+      email: new FormControl(teacher.email),
+      status: new FormControl(teacher.status),
+    });
+
+    let editModal: any;
+    editModal = document.getElementById('editModal');
+    editModal.style.display="block";
+  };
 
 }

d204c4b0780ddf739d00fe78c624fc63eb41a702
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-13 11:25:29 +0200

    add funcionallity to manage users

--- src/app/pages/manage-users/manage-users.component.ts
+++ src/app/pages/manage-users/manage-users.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-users',
@@ -11,6 +12,9 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './manage-users.component.scss'
 })
 export class ManageUsersComponent {
+
+  deleteVariables: any = {};
+  charge: boolean = false;
 
   users: any = [
     {
@@ -40,13 +44,141 @@ export class ManageUsersComponent {
   addUserForm = new FormGroup({
     name: new FormControl(""),
     surname: new FormControl(""),
-    mobil: new FormControl(""),
+    mobile: new FormControl(""),
     email: new FormControl(""),
     city: new FormControl(""),
-    text: new FormControl(""),
     permissions: new FormControl(""),
     position: new FormControl(""),
     status: new FormControl(""),
   });
+
+  editUserForm = new FormGroup({
+    name: new FormControl(""),
+    surname: new FormControl(""),
+    mobile: new FormControl(""),
+    email: new FormControl(""),
+    city: new FormControl(""),
+    permissions: new FormControl(""),
+    position: new FormControl(""),
+    status: new FormControl(""),
+  });
+
+  constructor(private http: HttpClient) {}
+
+  addUser(){
+    let user: any = {
+      name: this.addUserForm.value.name,
+      surname: this.addUserForm.value.surname,
+      mobile: this.addUserForm.value.mobile,
+      email: this.addUserForm.value.email,
+      city: this.addUserForm.value.city,
+      permissions: this.addUserForm.value.permissions,
+      position: this.addUserForm.value.position,
+      status: this.addUserForm.value.status,
+    };
+    
+    console.log(this.addUserForm.value);
+
+    this.http.post<any>('http://localhost:4000/api/user', user).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        //alert('Cargar fallo' + err);
+      },
+      });
+  };
+
+  openDeleteModal(user: any){
+    let deleteModal: any;
+    deleteModal = document.getElementById('deleteModal');
+    deleteModal.style.display="block";
+    this.deleteVariables = user;
+  };
+
+  delete(){
+    console.log(this.deleteVariables.id)
+    this.http.delete<any>('http://localhost:4000/api/user', this.deleteVariables.id).subscribe({
+      next: (res) => {
+        console.log(res)
+        let deleteModal: any;
+        deleteModal = document.getElementById('deleteModal');
+        deleteModal.style.display="none";
+      },
+      error: (err) => {
+        //alert('Cargar fallo' + err);
+      },
+    });
+  };
+
+  closeEditModal(){
+    let editModal: any;
+    editModal = document.getElementById('editModal');
+    editModal.style.display="none";
+  };
+
+  closeDeleteModal(){
+    let deleteModal: any;
+    deleteModal = document.getElementById('deleteModal');
+    deleteModal.style.display="none";
+  };
+
+  modify(){
+
+    let mod: any = {
+      name: this.editUserForm.value.name,
+      surname: this.editUserForm.value.surname,
+      mobile: this.editUserForm.value.mobile,
+      status: this.editUserForm.value.status,
+      email: this.editUserForm.value.email,
+      city: this.editUserForm.value.city,
+      permissions: this.editUserForm.value.permissions,
+      position: this.editUserForm.value.position,
+      action: "modify"
+    };
+  
+    this.http.put<any>('http://localhost:4000/api/user', mod).subscribe({
+      next: (res) => {
+        console.log(res)
+        let editModal: any;
+        editModal = document.getElementById('editModal');
+        editModal.style.display="none";
+      },
+      error: (err) => {
+        //alert('Cargar fallo' + err);
+      },
+    });
+  };
+
+  openEditModal(user: any){
+    this.editUserForm = new FormGroup({
+      name: new FormControl(user.name),
+      surname: new FormControl(user.surname),
+      mobile: new FormControl(user.mobile),
+      email: new FormControl(user.email),
+      city: new  FormControl(user.city),
+      permissions: new FormControl(user.permissions),
+      position: new FormControl(user.position),
+      status: new FormControl(user.status),
+    });
+
+    let editModal: any;
+    editModal = document.getElementById('editModal');
+    editModal.style.display="block";
+  };
+
+  desactivate(user: any){
+    let des: any = {};
+    des.id = user.id;
+    des.action = "desactivate";
+    this.http.put<any>('http://localhost:4000/api/user', des).subscribe({
+      next: (res) => {
+        console.log(res)
+      },
+      error: (err) => {
+        //alert('Cargar fallo' + err);
+      },
+    });
+  };
 
 }

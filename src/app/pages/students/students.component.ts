1b4ac237bbb8cd8c9df7fff729be4c8f9727a504
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-14 16:05:45 +0200

    end day

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-students',
@@ -11,83 +12,26 @@ import { FooterComponent } from '../../components/footer/footer.component';
 })
 export class StudentsComponent {
 
-  students: any = [
-    {
-      id: 1,
-      name: "José Carlos",
-      surname: "Fernández Giménez",
-      document: "x9865553214x",
-      birthday: "2000-03-03",
-      mobile: "+659635214587",
-      email: "marketingassistant@ailmadr55id.com",
-      status: true,
-      city: "Vallecas",
-      genre: "hombre",
-      classes: [
-        {
-          id: 4,
-          number_students:7,
-          level: "A2",
-          type: "full time",
-          teacher: "María Fernanda",
-          status: true,
-        },
-        {
-          id: 5,
-          number_students: 8,
-          level: "Otro",
-          type: "cocina",
-          teacher: "María Fernanda",
-          status: true,
-        },
-        {
-          id: 6,
-          number_students: 19,
-          level: "Otro",
-          type: "ocio",
-          teacher: "María Fernanda",
-          status: false,
-        }
-      ]
-    },
-    {
-      id: 2,
-      name: "María Fernanda",
-      surname: "González Perez",
-      document: "x9865557414x",
-      birthday: "2000-03-03",
-      mobile: "+654985645132",
-      email: "marketingassistant@ailma55drid.com",
-      status: false,
-      city: "Principado de Mónaco",
-      genre: "mujer",
-      classes: [
-        {
-          id: 1,
-          number_students: 4,
-          level: "A2",
-          type: "full time",
-          teacher: "María Fernanda",
-          status: true,
-        },
-        {
-          id: 2,
-          number_students: 10,
-          level: "A2",
-          type: "cocina",
-          teacher: "María Fernanda",
-          status: true,
-        },
-        {
-          id: 3,
-          number_students: 100,
-          level: "A2",
-          type: "personalizadas",
-          teacher: "María Fernanda",
-          status: true,
-        }
-      ]
-    }
-  ];
+  students: any = {};
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.load();
+  }
+  
+  load(){
+    this.http.get<any>('http://localhost:4000/api/alumnos').subscribe({
+      next: (res) => {
+
+        res.forEach((element: any) => {
+          element.birth_date = new Date(element.birth_date).toLocaleDateString()
+        });
+        this.students = res;
+      },
+      error: (err) => {
+        alert('Cargar fallo' + err);
+      },
+    });
+  };
 }

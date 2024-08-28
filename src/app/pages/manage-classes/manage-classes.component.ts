805d19c0ccb8217ebad42e957b32cb2b070b2650
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-28 10:09:48 +0200

    get db added manage classes

--- src/app/pages/manage-classes/manage-classes.component.ts
+++ src/app/pages/manage-classes/manage-classes.component.ts
@@ -16,213 +16,26 @@ export class ManageClassesComponent {
   deleteVariables: any = {};
   charge: boolean = false;
 
-  classes: any = [
-    {
-      id: 4,
-      number_students:7,
-      level: "A2",
-      type: "full time",
-      teacher: "María Fernanda",
-      status: true,
-      students: [
-        {
-          id: 1,
-          name: "José Carlos",
-          surname: "Fernández Giménez",
-          document: "x9865553214x",
-          birthday: "2000-03-03",
-          mobile: "+659635214587",
-          email: "marketingassistant@ailmadr55id.com",
-          status: true,
-          city: "Vallecas",
-          genre: "hombre"
-        },
-        {
-          id: 2,
-          name: "María Fernanda",
-          surname: "González Perez",
-          document: "x9865557414x",
-          birthday: "2000-03-03",
-          mobile: "+654985645132",
-          email: "marketingassistant@ailma55drid.com",
-          status: false,
-          city: "Principado de Mónaco",
-          genre: "mujer"
-        }
-      ]
-    },
-    {
-      id: 5,
-      number_students: 8,
-      level: "Otro",
-      type: "cocina",
-      teacher: "María Fernanda",
-      status: true,
-      students: [
-        {
-          id: 1,
-          name: "José Carlos",
-          surname: "Fernández Giménez",
-          document: "x9865553214x",
-          birthday: "2000-03-03",
-          mobile: "+659635214587",
-          email: "marketingassistant@ailmadr55id.com",
-          status: true,
-          city: "Vallecas",
-          genre: "hombre"
-        },
-        {
-          id: 2,
-          name: "María Fernanda",
-          surname: "González Perez",
-          document: "x9865557414x",
-          birthday: "2000-03-03",
-          mobile: "+654985645132",
-          email: "marketingassistant@ailma55drid.com",
-          status: false,
-          city: "Principado de Mónaco",
-          genre: "mujer"
-        }
-      ]
-    },
-    {
-      id: 6,
-      number_students: 19,
-      level: "Otro",
-      type: "ocio",
-      teacher: "María Fernanda",
-      status: false,
-      students: [
-        {
-          id: 1,
-          name: "José Carlos",
-          surname: "Fernández Giménez",
-          document: "x9865553214x",
-          birthday: "2000-03-03",
-          mobile: "+659635214587",
-          email: "marketingassistant@ailmadr55id.com",
-          status: true,
-          city: "Vallecas",
-          genre: "hombre"
-        },
-        {
-          id: 2,
-          name: "María Fernanda",
-          surname: "González Perez",
-          document: "x9865557414x",
-          birthday: "2000-03-03",
-          mobile: "+654985645132",
-          email: "marketingassistant@ailma55drid.com",
-          status: false,
-          city: "Principado de Mónaco",
-          genre: "mujer"
-        }
-      ]
-    },
-    {
-      id: 1,
-      number_students: 4,
-      level: "A2",
-      type: "full time",
-      teacher: "María Fernanda",
-      status: true,
-      students: [
-        {
-          id: 1,
-          name: "José Carlos",
-          surname: "Fernández Giménez",
-          document: "x9865553214x",
-          birthday: "2000-03-03",
-          mobile: "+659635214587",
-          email: "marketingassistant@ailmadr55id.com",
-          status: true,
-          city: "Vallecas",
-          genre: "hombre"
-        },
-        {
-          id: 2,
-          name: "María Fernanda",
-          surname: "González Perez",
-          document: "x9865557414x",
-          birthday: "2000-03-03",
-          mobile: "+654985645132",
-          email: "marketingassistant@ailma55drid.com",
-          status: false,
-          city: "Principado de Mónaco",
-          genre: "mujer"
-        }
-      ]
-      },
-    {
-      id: 2,
-      number_students: 10,
-      level: "A2",
-      type: "cocina",
-      teacher: "María Fernanda",
-      status: true,
-      students: [
-        {
-          id: 1,
-          name: "José Carlos",
-          surname: "Fernández Giménez",
-          document: "x9865553214x",
-          birthday: "2000-03-03",
-          mobile: "+659635214587",
-          email: "marketingassistant@ailmadr55id.com",
-          status: true,
-          city: "Vallecas",
-          genre: "hombre"
-        },
-        {
-          id: 2,
-          name: "María Fernanda",
-          surname: "González Perez",
-          document: "x9865557414x",
-          birthday: "2000-03-03",
-          mobile: "+654985645132",
-          email: "marketingassistant@ailma55drid.com",
-          status: false,
-          city: "Principado de Mónaco",
-          genre: "mujer"
-        }
-      ]
-    },
-    {
-      id: 3,
-      number_students: 100,
-      level: "A2",
-      type: "personalizadas",
-      teacher: "María Fernanda",
-      status: true,
-      students: [
-        {
-          id: 1,
-          name: "José Carlos",
-          surname: "Fernández Giménez",
-          document: "x9865553214x",
-          birthday: "2000-03-03",
-          mobile: "+659635214587",
-          email: "marketingassistant@ailmadr55id.com",
-          status: true,
-          city: "Vallecas",
-          genre: "hombre"
-        },
-        {
-          id: 2,
-          name: "María Fernanda",
-          surname: "González Perez",
-          document: "x9865557414x",
-          birthday: "2000-03-03",
-          mobile: "+654985645132",
-          email: "marketingassistant@ailma55drid.com",
-          status: false,
-          city: "Principado de Mónaco",
-          genre: "mujer"
-        }
-      ]
-    },
-  ];
+  classes: any = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.load();
+  }
+  
+  load(){
+    this.http.get<any>('http://localhost:4000/api/classes').subscribe({
+      next: (res) => {
+        this.classes = res;
+        console.log(res);
+      },
+      error: (err) => {
+        alert('Cargar fallo' + err);
+      },
+    });
+  };
+  
   addClassesForm = new FormGroup({
     teacher: new FormControl(""),
     level: new FormControl(""),
@@ -236,8 +49,6 @@ export class ManageClassesComponent {
     type: new FormControl(""),
     status: new FormControl("")
   });
-
-  constructor(private http: HttpClient) {}
 
   addClass(){
     let classes: any = {

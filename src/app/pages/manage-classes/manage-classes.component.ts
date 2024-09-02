d0ca4665933d084a8e4c971ae7cdd11b70a9a0c2
AilItAssistant <marketingassistant@ailmadrid.com>
2024-09-02 10:24:16 +0200

    manage classes order

--- src/app/pages/manage-classes/manage-classes.component.ts
+++ src/app/pages/manage-classes/manage-classes.component.ts
@@ -20,6 +20,10 @@ export class ManageClassesComponent {
 
   constructor(private http: HttpClient) {}
 
+  orderForm = new FormGroup({
+    select: new FormControl(""),
+  });
+
   ngOnInit() {
     this.load();
   }
@@ -152,6 +156,73 @@ export class ManageClassesComponent {
         //alert('Cargar fallo' + err);
       },
     });
+  };
+
+  order(){
+    switch(this.orderForm.value.select){
+      case "":
+        break;
+      case "lastName_asc":
+        this.classes.sort( (a:any, b:any) => {
+          if (a.teacher_last_name > b.teacher_last_name) {
+            return 1;
+          }
+          if (a.teacher_last_name < b.teacher_last_name) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "lastName_desc":
+        this.classes.sort( (a:any, b:any) => {
+          if (a.teacher_last_name < b.teacher_last_name) {
+            return 1;
+          }
+          if (a.teacher_last_name > b.teacher_last_name) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "level_asc":
+          this.classes.sort( (a:any, b:any) => {
+          if (a.class_level > b.class_level) {
+            return 1;
+          }
+          if (a.class_level < b.class_level) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "level_desc":
+        this.classes.sort( (a:any, b:any) => {
+          if (a.class_level < b.class_level) {
+            return 1;
+          }
+          if (a.class_level > b.class_level) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "className_asc":
+        this.classes.sort( (a:any, b:any) => {
+          if (a.class_name > b.class_name) {
+            return 1;
+          }
+          if (a.class_name < b.class_name) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "className_desc":
+        this.classes.sort( (a:any, b:any) => {
+          if (a.class_name < b.class_name) {
+            return 1;
+          }
+          if (a.class_name > b.class_name) {
+            return -1;
+          }
+          return 0});
+        break;
+    };
   };
 
 }

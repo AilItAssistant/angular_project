32770438916d2094f348fbab148bbeabe357a3b7
AilItAssistant <marketingassistant@ailmadrid.com>
2024-09-02 09:55:15 +0200

    students order

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import {FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './students.component.html',
   styleUrl: './students.component.scss'
 })
@@ -15,6 +16,10 @@ export class StudentsComponent {
   students: any;
 
   constructor(private http: HttpClient) {}
+
+  orderForm = new FormGroup({
+    select: new FormControl(""),
+  });
 
   ngOnInit() {
     this.load();
@@ -30,4 +35,53 @@ export class StudentsComponent {
       },
     });
   };
+
+  order(){
+    console.log(this.students)
+    switch(this.orderForm.value.select){
+      case "":
+        break;
+      case "lastName_asc":
+        this.students.sort( (a:any, b:any) => {
+          if (a.last_name > b.last_name) {
+            return 1;
+          }
+          if (a.last_name < b.last_name) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "lastName_desc":
+        this.students.sort( (a:any, b:any) => {
+          if (a.last_name < b.last_name) {
+            return 1;
+          }
+          if (a.last_name > b.last_name) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "city_asc":
+          this.students.sort( (a:any, b:any) => {
+          if (a.city > b.city) {
+            return 1;
+          }
+          if (a.city < b.city) {
+            return -1;
+          }
+          return 0});
+        break;
+      case "city_desc":
+        this.students.sort( (a:any, b:any) => {
+          if (a.city < b.city) {
+            return 1;
+          }
+          if (a.city > b.city) {
+            return -1;
+          }
+          return 0});
+        break;
+    };
+  };
+
 }

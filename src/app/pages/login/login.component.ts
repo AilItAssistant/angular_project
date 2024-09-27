4a6f6dfc7bbbda8787713a810779b17b49ed1a1c
AilItAssistant <marketingassistant@ailmadrid.com>
2024-09-27 15:56:42 +0200

    end day

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,8 +18,34 @@ export class LoginComponent {
     password: new FormControl(""),
   });
 
-  login(){
-    console.log(this.loginForm.value)
-  };
+  constructor(
+    private http: HttpClient, 
+    private router: Router, 
+    private authService: AuthService) {}
 
+  login(){
+    let credentials: any = {
+      user: this.loginForm.value.user,
+      pass: this.loginForm.value.password,
+    };
+
+    /*this.authService.singin(credentials).subscribe( (res:any) => {
+      console.log(res);
+      localStorage.setItem('token',res.token);
+      this.router.navigate(['']);
+      let form: any = document.getElementById("loginForm");
+      form.reset();
+    })*/
+
+    this.http.post<any>('http://localhost:4000/api/users/login', credentials).subscribe({
+      next: (res) => {
+        console.log(res)
+        localStorage.setItem('token',res.token);
+        this.router.navigate([''])
+      },
+      error: (err) => {
+        alert('Cargar fallo' + err);
+      },
+    });
+  };
 }
--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from '../../components/header/header.component';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, ReactiveFormsModule, HeaderComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
+  signinForm = new FormGroup({
+    user: new FormControl(""),
+    password: new FormControl(""),
+  });
 
+  constructor(private http: HttpClient) {}
+
+  signin(){
+    let credentials: any = {
+      user: this.signinForm.value.user,
+      pass: this.signinForm.value.password,
+    }
+
+    console.log(credentials)
+
+    /*this.http.post<any>('http://localhost:4000/api/users/signin', credentials).subscribe({
+      next: (res) => {
+        console.log(res);
+        let form: any = document.getElementById("signinForm");
+        form.reset();
+      },
+      error: (err) => {
+        alert('Cargar fallo' + err);
+      },
+    });*/
+
+    this.http.get<any>('http://localhost:4000/api/classes').subscribe({
+      next: (res) => {
+        console.log(res)
+      },
+      error: (err) => {
+        alert('Cargar fallo' + err);
+      },
+    });
+
+    console.log('works')
+  };
 }
